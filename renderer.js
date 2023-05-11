2c99130e37b0a623cf097c16349bf24ea5fb5b96
butzee <butzee@users.noreply.github.com>
2023-05-11 15:34:42 +0200

    Not needed anymore

--- renderer.js
+++ renderer.js
@@ -1,9 +1,3 @@
-document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
-    const isDarkMode = await window.darkMode.toggle()
-    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
-})
-  
-
 document.addEventListener("DOMContentLoaded", async () => {
     console.log("Render test");
 
