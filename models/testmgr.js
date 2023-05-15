27775a7f4271e449acb9e9249fdef3e5def98275
butzee <butzee@users.noreply.github.com>
2023-05-15 23:28:55 +0200

    Big Update: - Display all years via slider - Animation between slider works TODO: Fetch and update 1992 after one runthrough

--- models/testmgr.js
+++ models/testmgr.js
@@ -1,14 +1,34 @@
 var dbmgr = require('./dbmgr');
 var db = dbmgr.db;
 
-exports.getShapes = () =>{
-    const sql = "SELECT * FROM fires LIMIT 10";
-    let stmt = db.prepare(sql);
-    let res = stmt.all();
-    db.all(sql, (err, rows) => {
-        rows.forEach(row => {
-            console.log(row)
+
+// return all fetched rows
+exports.getShapes = (sliderValue) => {
+    var year;
+    if (sliderValue == NaN || sliderValue == undefined || sliderValue == null || sliderValue == 0) {
+      year = parseInt(1992);
+    } else {
+      year = parseInt(1992) + parseInt(sliderValue);
+    }
+    return new Promise((resolve, reject) => {
+      fetchDataFromDatabase("SELECT OBJECTID, DISCOVERY_DOY, CONT_DOY, FIRE_SIZE, LONGITUDE, LATITUDE FROM Fires WHERE FIRE_YEAR='"+year+"' LIMIT 10000")
+        .then(rows => {
+          resolve(rows);
+        })
+        .catch(error => {
+          reject(error);
         });
     });
-    return res;
-}
+  };
+
+function fetchDataFromDatabase(sql) {
+    return new Promise((resolve, reject) => {
+      db.all(sql, (err, rows) => {
+        if (err) {
+          reject(err); // Reject the Promise if there's an error
+        } else {
+          resolve(rows); // Resolve the Promise with the fetched rows
+        }
+      });
+    });
+  }
