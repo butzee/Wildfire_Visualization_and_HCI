2ed8ee6b1a48121bbede6133811c0689899ea659
butzee <butzee@users.noreply.github.com>
2023-05-20 17:28:26 +0200

    Path for DB

--- models/dbmgr.js
+++ models/dbmgr.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 
-let db = new sqlite3.Database('./data/data.sqlite', sqlite3.OPEN_READONLY, (err) => {
+let db = new sqlite3.Database('./data.sqlite', sqlite3.OPEN_READONLY, (err) => {
     if (err) {
         console.error(err.message);
     } else {
