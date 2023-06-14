2701b13209eab2e6d322602dbc1b96c87c4e2522
butzee <butzee@users.noreply.github.com>
2023-06-14 16:48:44 +0200

    Compile ready windows

--- src/models/dbmgr.js
+++ src/models/dbmgr.js
@@ -1,8 +1,14 @@
+const path = require('path');
+const electron = require('electron');
+
+// Get the path to the database file
+const dbFilePath = path.join(electron.app.getAppPath(), '..', 'data.sqlite');
+console.log(dbFilePath)
+
 // Connecting to SQLite Database
 const sqlite3 = require('sqlite3').verbose();
 
-
-let db = new sqlite3.Database('./data.sqlite', sqlite3.OPEN_READONLY, (err) => {
+let db = new sqlite3.Database(dbFilePath, sqlite3.OPEN_READONLY, (err) => {
     if (err) {
         console.error(err.message);
     } else {
